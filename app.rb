efed55265eeabc7c80ece2e44cbad474fceb485e
Francesco Rodriguez <frodsan@me.com>
2014-03-02 23:55:17 -0300

    Update main file.

--- app.rb
+++ app.rb
@@ -5,19 +5,25 @@ require "rack/protection"
 require "shield"
 require "scrivener"
 
+APP_KEY = ENV.fetch("APP_KEY")
+APP_SECRET = ENV.fetch("APP_SECRET")
+REDIS_URL = ENV.fetch("REDIS_URL")
+
 Cuba.plugin(Cuba::Mote)
 Cuba.plugin(Shield::Helpers)
 
-Dir["./lib/**/*.rb"].each     { |f| require(f) }
-Dir["./models/**/*.rb"].each  { |f| require(f) }
-Dir["./filters/**/*.rb"].each { |f| require(f) }
-Dir["./helpers/**/*.rb"].each { |f| require(f) }
-Dir["./routes/**/*.rb"].each  { |f| require(f) }
+Cuba.use(Rack::Session::Cookie, key: APP_KEY, secret: APP_SECRET)
+Cuba.use(Rack::Static, urls: %w[/js /css /img], root: "./public")
 
 Cuba.use(Rack::Protection)
 Cuba.use(Rack::Protection::RemoteReferrer)
 
-Cuba.use(Rack::Static, urls: %w[/js /css /img], root: "./public")
+Ohm.redis = Redic.new(REDIS_URL)
+
+Dir["./models/**/*.rb"].each  { |f| require(f) }
+Dir["./filters/**/*.rb"].each { |f| require(f) }
+Dir["./helpers/**/*.rb"].each { |f| require(f) }
+Dir["./routes/**/*.rb"].each  { |f| require(f) }
 
 Cuba.define do
 end
